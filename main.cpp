e4405a07f3a2d91d506c505549d036ef6764627f
Yorlko <petrzhdanov2004@gmail.com>
2022-09-06 11:18:29 +0300

    complete

--- main.cpp
+++ main.cpp
@@ -13,53 +13,103 @@ int main() {
     in.open(file_path);
     getline(in, stroke);
 
-    for (auto chr : stroke) {
-        switch (chr) {
+    do {
+        switch (stroke[cnt]) {
             case '(' :
-                scobs.push(chr);
+                scobs.push(stroke[cnt]);
                 break;
             case '{' :
-                scobs.push(chr);
+                scobs.push(stroke[cnt]);
                 break;
             case '[' :
-                scobs.push(chr);
-                break;  
-        }
-    }
-    if (scobs.empty()) {
-        cout << '0';
-        return 0;
-    }
-    cnt = 0;
-    for (auto chr : stroke) {
-        switch (chr) {
+                scobs.push(stroke[cnt]);
+                break;
             case ')' :
-                if(!scobs.empty() && scobs.top() == '(') {
+                if (!scobs.empty() && scobs.top() == '(') {
                     scobs.pop();
+                    break;
                 } else {
                     cout << cnt;
                     return 0;
                 }
-                break;
             case '}' :
-                if(!scobs.empty() && scobs.top() == '{') {
+                if (!scobs.empty() && scobs.top() == '{') {
                     scobs.pop();
+                    break;
                 } else {
                     cout << cnt;
                     return 0;
                 }
-                break;
             case ']' :
-                if(!scobs.empty() && scobs.top() == '[') {
+                if (!scobs.empty() && scobs.top() == '[') {
                     scobs.pop();
+                    break;
                 } else {
                     cout << cnt;
                     return 0;
-                }
-                break;
+                } 
         }
         cnt++;
-    }
+    } while (!scobs.empty());
+
+    // for (auto chr : stroke) {
+    //     switch (chr) {
+    //         case '(' :
+    //             scobs.push(chr);
+    //             break;
+    //         case '{' :
+    //             scobs.push(chr);
+    //             break;
+    //         case '[' :
+    //             scobs.push(chr);
+    //             break;  
+    //     }
+    // }
+    // if (scobs.empty()) {
+    //     cout << '0';
+    //     return 0;
+    // }
+    // cnt = 0;
+    // char chr = 0;
+    // for (int i = sizeof(stroke) - 1; i >= 0; i--) {
+    //     chr = stroke[i];
+    //     switch (chr) {
+    //         case ')' :
+    //             if(!scobs.empty() && scobs.top() == '(') {
+    //                 scobs.pop();
+    //             } else {
+    //                 // if (stroke[cnt] == '{' || stroke[cnt] == '[') {
+    //                 //     cnt++;
+    //                 // }
+    //                 cout << i;
+    //                 return 0;
+    //             }
+    //             break;
+    //         case '}' :
+    //             if(!scobs.empty() && scobs.top() == '{') {
+    //                 scobs.pop();
+    //             } else {
+    //                 // if (stroke[cnt] == '(' || stroke[cnt] == '[') {
+    //                 //     cnt++;
+    //                 // }
+    //                 cout << i;
+    //                 return 0;
+    //             }
+    //             break;
+    //         case ']' :
+    //             if(!scobs.empty() && scobs.top() == '[') {
+    //                 scobs.pop();
+    //             } else {
+    //                 // if (stroke[cnt] == '{' || stroke[cnt] == '(') {
+    //                 //     cnt++;
+    //                 // }
+    //                 cout << i;
+    //                 return 0;
+    //             }
+    //             break;
+    //     }
+    //     cnt = i;
+    // }
 
     if (scobs.empty()) {
         cout << "Succes";
